58c8bc7ba037adceb154e22736cb3365c0b7409b
usman <usmanahmed1100@gmail.com>
2020-08-09 23:03:42 +0500

    Fixed issue: No results were showing on 1st search

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import CardContainer from './components/CardContainer';
 import Card from './components/Card';
 import Spinner from "./components/Spinner"
 import NotFoundText from './components/NotFoundText';
+import Alert from './components/Alert';
 
 
 
@@ -18,11 +19,23 @@ class App extends React.Component {
             userList: [],
             userName: "",
             loading: false,
-            notFound: false
+            notFound: false,
+            alert: false
         }
     }
 
     getUsersList = userName => {
+
+
+        // If user enters nothing
+        // Alert will be set to true inorder...
+        // ...to display the <Alert/> component
+        // NotFound will be set to true just ...
+        // ...  to get rid of it on that time
+        if (this.state.userName === "") {
+            this.setState({ alert: true });
+            return;
+        }
 
         this.setState({ loading: true })
         const url = `https://api.github.com/search/users?q=${this.state.userName}`
@@ -30,49 +43,69 @@ class App extends React.Component {
         fetch(url)
             .then(response => response.json())
             .then(data => {
-                this.setState({ userList: data['items'] })
-                if (this.state.userList.length === 0){
-                    console.log("Empty Array")
-                    this.setState({ notFound: true,loading: false })
-                }
-                
-                else
-                    this.setState({ loading: false, notFound:false })
+                console.log(data['items'], "Data")
+                this.setState({ userList: data['items'] }, () => {
 
-                
-                console.log(this.state.userList)
-                
+
+                    // Making sure the userList value
+                    // is an array in all cases so it doesnt throw
+                    // an error ever
+                    if (this.state.userList === undefined)
+                        this.setState({ userList: [] })
+
+                    if (this.state.userList.length === 0) {
+                        // Incase of error
+                        // i.e Request fails or user is not found
+                        this.setState({ notFound: true, loading: false, alert: false })
+                    }
+
+                    else {
+                        // If everything goes smoothly
+                        this.setState({ loading: false, notFound: false, alert: false })
+                    }
+                })
 
             })
+
+
+        console.log(this.state.userList ,"Userlist")
     }
 
     setUserName = e => {
+        // Updating the username value
+        // obtained from the SearchBox component
         this.setState({ userName: e.target.value }
         )
 
     }
 
     render() {
-        const {notFound, loading} = this.state;
+        const { notFound, loading, alert } = this.state;
         return (
             <div className="App">
 
                 <Header />
 
+                {/* If user has not entered anything */}
+                {alert && <Alert />}
+
                 <SearchBox updateUserName={this.setUserName} />
 
                 <SearchButton getUsers={this.getUsersList} />
 
-                {loading ? <Spinner /> : notFound?<NotFoundText/>:
-                    <CardContainer>
-                        {
-                            
-                            this.state.userList.map(user =>
-                                <Card key={user.id} profile={user} />
-                            )
-                            
-                        }
-                    </CardContainer>
+                {
+                    loading ? <Spinner />
+                        : notFound ? <NotFoundText /> :
+                            <CardContainer>
+                                {
+
+                                    this.state.userList.map(user =>
+                                        <Card key={user.id} profile={user} />
+                                    )
+
+                                }
+                            </CardContainer>
+
 
                 }
 
---
+++ src/components/Alert.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Alert = () => {
+    return (
+        <div className="container">
+            <div className="alert alert-danger" role="alert">
+                Enter something
+            </div>
+        </div>
+    );
+};
+
+export default Alert;
